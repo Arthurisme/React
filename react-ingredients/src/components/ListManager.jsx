4c5c9afaf2602c5fcc1acdcb3a016ff6dbf21aa8
Arthurisme <is@live.ca>
2016-02-13 09:17:22 -0500

    201602130823

---
+++ react-ingredients/src/components/ListManager.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+
+var ListManager = React.createClass({
+
+getInitialState: function(){
+    return {}
+},
+
+
+
+
+
+{/*  render: function(){
+    return(
+      <li>
+        <h4>
+          {this.props.text}
+        </h4>
+      </li>
+    );
+  }*/}
+
+
+
+});
+
+
+module.exports = ListItem;
