7eb16b76ec860ce4c3dccf9854a32f1ad229113d
Arthurisme <is@live.ca>
2016-02-13 18:25:18 -0500

    201602131823

---
+++ ReactbyFI/ReactbyFI-/project/03-003-list-by-map-bootstrap/timer.jsx
@@ -0,0 +1,87 @@
+
+
+var TimerWrapper =React.createClass({
+
+    getInitialState:function(){
+        return {time: null, int:null}
+    },
+
+
+    startTimer:function(time){
+        //TODO:set Interval
+        var _this = this
+        clearInterval(this.state.int)
+        var int = setInterval(function(){
+            var tl =_this.state.time - 1
+            if (tl == 0 ) clearInterval(int)
+            _this.setState({time:tl})
+        },1000)
+
+        return this.setState({time:time,int:int})
+    },
+
+
+
+
+
+
+    render: function (){
+
+        return  (
+            <div>
+                <h2>Timer</h2>
+                <Button time="5" startTimer={this.startTimer}/>
+                <Button time="10" startTimer={this.startTimer}/>
+                <Button time="15" startTimer={this.startTimer}/>
+                <Timer time ={this.state.time}/>
+
+            </div>
+
+
+        );
+
+    }
+
+})
+
+
+
+var Button =React.createClass({
+
+    startTimer: function(){
+        return this.props.startTimer(this.props.time)
+    },
+
+    render: function (){
+
+        return  <p onClick={this.startTimer}>{this.props.time} seconds </p>
+
+
+    }
+
+})
+
+
+
+
+
+var Timer =React.createClass({
+
+
+    render: function (){
+
+        if (this.props.time == null ) return <div />
+
+
+        return(
+            <h1>{this.props.time}</h1>
+        );
+    }
+})
+
+React.render(
+
+    <TimerWrapper />,
+    document.getElementById("example")
+);
+
